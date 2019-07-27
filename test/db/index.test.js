08e03f95137498d6cfddd58d872a31b78945c6ad
Fakhriddin Umarov <f.umarov@stylemix.net>
2019-07-27 21:21:07 +0500

    fix db connection test

--- test/db/index.test.js
+++ test/db/index.test.js
@@ -4,9 +4,8 @@ const db = require('../../src/db')
 afterAll(() => db.destroy())
 
 describe('Database', () => {
-  it('should connect successfully', () => {
-    return expect(db.first('product_id').from('product')).resolves.toEqual({
-      product_id: 1,
-    })
+  it('should connect successfully', async () => {
+    const { result } = (await db.raw('select 1 + 1 as result'))[0][0]
+    expect(result).toBe(2)
   })
 })
