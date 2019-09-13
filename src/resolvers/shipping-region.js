45f80d7e52e478d01f75aa438185de6eaa4092ca
Fakhriddin Umarov <f.umarov@stylemix.net>
2019-09-13 21:47:15 +0500

    add shipping region tax resolver

--- src/resolvers/shipping-region.js
+++ src/resolvers/shipping-region.js
@@ -1,6 +1,18 @@
+/* eslint-disable camelcase */
 module.exports = {
-  // eslint-disable-next-line camelcase
   shippingTypes({ shipping_region_id }, args, { loaders }) {
     return loaders.shippingRegion.shippingTypes.load(shipping_region_id)
+  },
+
+  tax({ shipping_region_id }, args, { db }) {
+    // simple formula!!!
+    // accept tax only for US/Canada shipping regions
+
+    // 2 means US/Canada region
+    const tax_id = shipping_region_id === 2 ? 1 : 2
+    return db
+      .first()
+      .from('tax')
+      .where({ tax_id })
   },
 }
